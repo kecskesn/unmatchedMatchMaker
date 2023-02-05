6bbce68d8ee177d22260b866c2d5d36d3583af92
norbert.kecskes@gmail.com <norbert.kecskes@gmail.com>
2023-02-05 14:40:03 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const axios = require('axios');
+const heroes = require('./constants/heroes')
+
+const app = express();
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/heroes', (req, res) => {
+    res.json(heroes);
+});
+ 
+app.get('/matches', (req, res) => {
+    const hero = req.query.hero;
+    const encodedHero = encodeURIComponent(hero);
+    const numberOfPlays = req.query.numberOfPlays;
+    console.log(encodedHero);
+
+    axios.get(`https://www.umleague.net/api/analytics/getHeroResultsByMap?hero=${encodedHero}&campaignid=10000&organizerid=0`)
+        .then(apiRes => {
+            const repeatOpponent = apiRes.data.repeatOpponent.map(item => {
+                return {
+                    hero: item.hero,
+                    plays: item.queryOpponentOverall[0].plays,
+                    winPercent: parseFloat(item.queryOpponentOverall[0].winpercent)
+                };
+            });
+
+            const filteredRepeatOpponent = repeatOpponent.filter(item => item.plays >= numberOfPlays);
+            const sortedFilteredRepeatOpponent = filteredRepeatOpponent.sort((a, b) => b.winPercent - a.winPercent);
+            res.send({ sortedFilteredRepeatOpponent });
+        })
+        .catch(error => {
+            console.error(error);
+            res.status(500).send({ error: 'Failed to retrieve data from the API.' });
+        });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Listening on port ${port}...`);
+});
